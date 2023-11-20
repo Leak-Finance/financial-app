3a5b236bba5951c411bda9600f6f1ecf569eed9f
sayufu <lilifuye72@gmail.com>
2023-11-19 22:51:00 -0500

    feat: add employee dashboard

--- src/customer/pages/catalog.page.vue
+++ src/customer/pages/catalog.page.vue
@@ -1,9 +1,244 @@
 <script>
+import {VehicleRetailService} from "@/shared/services/vehicle-retail.service";
+import PostCatalogCard from "@/customer/components/post-catalog-card.component.vue";
+
 export default {
-  name: 'CatalogCustomersPage'
+  name: 'CatalogCustomersPage',
+  components: { PostCatalogCard},
+  data() {
+    return {
+      vehicleRetailService: new VehicleRetailService(),
+      itemsPerPage: 9,
+      currentPage: 1,
+      posts: [],
+      active : 0,
+      selectedPost: null,
+      selectedConfigurations: null,
+      items: [
+        {label: 'Seleciona un vehículo'},
+        {label: 'Compra Inteligente'},
+        {label: 'Solicita tu crédito'},
+      ],
+      compraInteligenteInfoDialog: false,
+
+      // Forms
+      currencies: [],
+      tasas: [{id: 1, name: "Nominal"}, {id: 2, name: "Efectiva"}],
+      plazosDeCredito: [{id: 1, cantidad: 24}, {id: 2, cantidad: 36}],
+      tiposPeriodosDeGracia: [{id: 1, nombre: "Total"}, {id: 2, nombre: "Parcial"}],
+
+      // Inputs for result
+      moneda: null,
+      cuotaInicial: null,
+      tipoTasaInteres: null,
+      tasaInteres: null,
+      plazoCredito: null,
+      tipoPeriodoGracia: null,
+      periodoGracia: null,
+
+      // Outputs
+      detallesCuotasDialog: false,
+    };
+  },
+  created() {
+    this.vehicleRetailService.getAllVehiclePosts().then((response) => {
+      this.posts = response.data;
+    });
+    this.vehicleRetailService.getAllCurrencies().then((response) => {
+      this.currencies = response.data;
+      console.log(this.currencies)
+    });
+  },
+  methods:{
+    calculateCurrentPageItems() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.posts.slice(startIndex, endIndex);
+    },
+    restart(){
+      this.selectedPost = null;
+      this.selectedConfigurations = null;
+      this.active = 0;
+    },
+    selectCar(post) {
+      this.selectedPost = post;
+      this.active = 1;
+    },
+    calculateRecommendedPrice(price) {
+      return "Recomendado 20% (" + this.selectedPost.currency.symbol + price * 0.2 + ")";
+    },
+    calculateCredit(){
+      // TODO: Calculate credit
+      const result = "TODO";
+      this.selectedConfigurations = result;
+    }
+  }
 }
 </script>
 
 <template>
-<p>Works</p>
+  <div class="container mx-auto">
+    <Steps v-model:activeStep="active" :model="items" />
+
+    <!-- Step 1: Select a car -->
+    <div v-if="!selectedPost" class="flex flex-col items-center gap-8 py-8">
+      <div v-if="posts.length > 0" class="flex flex-wrap justify-center gap-12">
+        <PostCatalogCard
+            v-for="post in calculateCurrentPageItems()"
+            :post="post"
+            :key="post.id"
+            :isSelected = false
+            @select="selectCar(post)"
+        />
+      </div>
+      <div v-else>
+        <p>No hay publicaciones</p>
+      </div>
+      <div class="flex gap-4">
+        <Button @click="currentPage -= 1" :disabled="currentPage === 1" icon="pi pi-angle-left" outlined severity="info" />
+        <Button @click="currentPage += 1" :disabled="currentPage === Math.ceil(posts.length / itemsPerPage)" icon="pi pi-angle-right" outlined severity="info" />
+      </div>
+    </div>
+
+    <!-- Step 2: -->
+    <div v-if="selectedPost">
+      <div class="w-full p-6 my-6 grid gap-4">
+        <div class="flex gap-4 items-center">
+          <Button icon="pi pi-arrow-left" text rounded aria-label="Filter" @click="restart" />
+          <p class=" font-medium text-primary">Crédito vehicular con Compra Inteligente</p>
+          <Button icon="pi pi-question-circle" text rounded aria-label="Filter" @click="compraInteligenteInfoDialog = true" />
+        </div>
+        <div class="grid md:grid-cols-2 gap-6 w-full">
+          <div class="gap-2">
+            <img :src="selectedPost.vehicle.photoUrl" alt="car" class="w-full">
+            <div class="flex gap-4 justify-between p-4 items-end border">
+              <div>
+                <p class="text-sm uppercase font-bold text-secondary">{{selectedPost.vehicle.brand.name}}</p>
+                <p class="text-xl font-medium">
+                  {{selectedPost.vehicle.brand.name}}&nbsp;{{ selectedPost.vehicle.model }}&nbsp;{{ selectedPost.vehicle.manufactureYear }}
+                </p>
+              </div>
+              <div>
+                <p class="font-bold text-xl text-primary">{{ selectedPost.currency.symbol }} {{ selectedPost.price }}</p>
+              </div>
+            </div>
+          </div>
+          <!-- FORM -->
+          <form v-if="selectedConfigurations === null"
+              class="border p-6 w-full grid gap-6" @submit.prevent="calculateCredit">
+            <p class="text-2xl font-medium text-primary">Precio de venta: {{ selectedPost.currency.symbol }} {{ selectedPost.price }}</p>
+            <div class="grid gap-2">
+              <p class="font-medium text-secondary">1. Selecciona moneda y cuota inicial</p>
+              <div class="grid md:flex md:gap-6 gap-2">
+                <div class="grid w-full">
+                  <label class="text-sm">Moneda</label>
+                  <Dropdown v-model="moneda"
+                            :options="currencies" optionLabel="symbol" placeholder="Selecciona moneda" class="w-full" />
+                </div>
+                <div class="grid w-full">
+                  <label class="text-sm">Cuota Inicial</label>
+                  <InputText v-model="cuotaInicial"
+                             aria-describedby="username-help" :placeholder="calculateRecommendedPrice(selectedPost.price)" />
+                </div>
+              </div>
+            </div>
+            <div class="grid gap-2">
+              <p class="font-medium text-secondary">2. Tasas de interés</p>
+              <div class="grid md:flex md:gap-6 gap-2">
+                <div class="grid w-full">
+                  <label class="text-sm">Tipo de tasa</label>
+                  <Dropdown v-model="moneda" :options="tasas"
+                            optionLabel="name" placeholder="Selecciona tipo de tasa" class="w-full" />
+                </div>
+                <div class="grid w-full">
+                  <label class="text-sm">Tasa de interés</label>
+                  <InputText v-model="tasaInteres"
+                             aria-describedby="username-help" />
+                </div>
+              </div>
+            </div>
+            lilitest@gmail.com<div class="grid gap-2">
+              <p class="font-medium text-secondary">3. Plazo de crédito</p>
+              <div class="grid md:flex md:gap-6 gap-2">
+                <div class="grid">
+                  <label class="text-sm">Plazo de crédito</label>
+                  <Dropdown v-model="plazoCredito"
+                            :options="plazosDeCredito"
+                            optionLabel="cantidad" placeholder="Selecciona plazo de créditos" class="w-full" />
+                </div>
+              </div>
+            </div>
+            <div class="grid gap-2">
+              <p class="font-medium text-secondary">4. Periodos de gracia</p>
+              <div class="grid md:flex md:gap-6 gap-2">
+                <div class="grid w-full">
+                  <label class="text-sm">Tipo de periodo de gracia</label>
+                  <Dropdown v-model="tipoPeriodoGracia"
+                            :options="tiposPeriodosDeGracia"
+                            optionLabel="nombre" placeholder="Selecciona tipo de periodo de gracia" class="w-full" />
+                </div>
+                <div class="grid w-full">
+                  <label class="text-sm">Periodos de gracia (meses)</label>
+                  <InputText v-model="periodoGracia"
+                             aria-describedby="username-help" />
+                </div>
+              </div>
+            </div>
+            <Button type="submit" label="Continuar" class="w-full md:w-1/2"  />
+          </form>
+
+
+          <!-- Step 3 -->
+          <div v-if="selectedConfigurations"
+               class="border w-full gap-6 p-6">
+            TODO: RESULTS
+
+            <div class="flex gap-4">
+              <Button label="Detalles" class="w-full" @click="detallesCuotas = true"/>
+              <Button label="Solicitar" class="w-full" severity="success" @click="detallesCuotas = true"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <Dialog v-model:visible="detallesCuotasDialog" modal
+          :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+          header="Detalles de cuota" >
+    TODO: Cuadro de cuotas
+
+  </Dialog>
+
+
+
+  <Dialog v-model:visible="compraInteligenteInfoDialog" modal
+          :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+          header="¿Qué es Compra Inteligente?" >
+    <div class="grid gap-4 text-lg">
+      <p>
+        Compra Inteligente es un tipo de crédito vehicular que, en este caso, estamos trabajando con los parámetros del Banco de Crédito del Perú (BCP).
+        Luego de cancelar una cuota inicial (recomendado: 20%), durante los siguientes 24 o 36 meses (de acuerdo al plazo pactado) se pagará una cuota mensual
+        por el capital de 40-30%* sobre el valor del auto más los intereses del monto total financiado. El valor de la última cuota será de 35-50% del precio del auto.
+      </p>
+      <p>
+        Entre las condiciones básicas de su banco para la emisión de un crédito vehicular de tipo Compra Inteligente son:
+      </p>
+      <ul>
+        <li>•Se debe tener un ingreso mensual bruto mínimo de S/ 1,500.</li>
+        <li>•Se debe afiliarse a un seguro vehicular durante el plazo total del crédito.</li>
+        <li>•Se debe tener un buen historial de comportamiento de pago en el BCP y sistemas financieros.</li>
+        <li>•No se debe contar con multas de últimos procesos electorales.</li>
+      </ul>
+      <p>
+        Para mayor detalle de Compra Inteligente vista la página oficial de BCP sobre:<br>
+        <span class="font-bold text-secondary underline">
+          <a href="https://www.viabcp.com/creditos/credito-vehicular/compra-inteligente" target="_blank">
+            Crédito Vehicular Inteligente BCP
+          </a>
+        </span>
+      </p>
+    </div>
+  </Dialog>
 </template>
--- src/employee/pages/dashboard.page.vue
+++ src/employee/pages/dashboard.page.vue
@@ -15,6 +15,7 @@ export default {
       currentPage: 1,
       itemsPerPage: 9,
       newCarPostDialog: false,
+      newVehicleDialog: false,
       posts: [],
       vehicles: [],
       vehicle_brands: [],
@@ -23,11 +24,18 @@ export default {
 
       errorMessage: '',
 
-      // New cars post dialog
+      // New post
       description: '',
       price: 0,
       vehicle: {},
       currency: {},
+
+
+      // New car
+      model: null,
+      manufactureYear: null,
+      photoUrl: null,
+      brand: 0,
     };
   },
   created() {
@@ -35,7 +43,7 @@ export default {
       this.vehicles = response.data;
     }).catch(error => {console.log(error);});
     this.vehicleRetailService.getAllVehiclePosts().then(response => {
-      this.posts = response.data;
+      this.posts = response.data.reverse();
     }).catch(error => {console.log(error);});
     this.vehicleRetailService.getAllVehicleBrands().then(response => {
       this.vehicle_brands = response.data;
@@ -76,6 +84,31 @@ export default {
         this.refreshPosts();
       }).catch(error => this.errorMessage = error.response.data.message[0]);
     },
+    createVehicle(){
+      if (!this.model || !this.manufactureYear || !this.photoUrl || !this.brand) {
+        this.errorMessage = 'Todos los campos son obligatorios';
+      }
+      else {
+        if (this.photoUrl) {
+          const url = this.photoUrl;
+          const ext = url.substring(url.lastIndexOf('.') + 1);
+          if (ext !== 'jpg' && ext !== 'png' && ext !== 'jpeg') {
+            this.errorMessage = 'La url de la foto debe con formato jpg, jpeg o png';
+          }
+          return;
+        }
+
+        this.vehicleRetailService.createVehicle(
+            this.model,
+            this.manufactureYear,
+            this.photoUrl,
+            this.brand.id
+        ).then(() => {
+          this.newVehicleDialog = false;
+          this.refreshPosts();
+        }).catch(error => this.errorMessage = error.response.data.message[0]);
+      }
+    },
     refreshPosts(){
       this.vehicleRetailService.getAllVehiclePosts().then(response => {
         this.posts = response.data;
@@ -91,10 +124,14 @@ export default {
 <template>
   <div class="container mx-auto">
     <div class="flex flex-col items-center gap-8 py-8">
-      <Button @click="newCarPostDialog=true"
-              label="Nueva publicación"
-              icon="pi pi-plus" severity="success"
-              class="w-full md:w-1/4" />
+      <div class="flex gap-4">
+        <Button @click="newCarPostDialog=true"
+                label="Nueva publicación"
+                icon="pi pi-plus" severity="success" />
+        <Button @click="newVehicleDialog=true"
+                label="Nuevo vehículo"
+                icon="pi pi-plus" />
+      </div>
       <div v-if="posts.length>0" class="flex flex-wrap justify-center gap-12">
         <PostManagementCard
             v-for="post in calculateCurrentPageItems()"
@@ -115,7 +152,36 @@ export default {
     </div>
   </div>
 
-  <Dialog v-model:visible="newCarPostDialog" header="Agregar carro" modal class="w-full md:w-1/3">
+
+  <Dialog v-model:visible="newVehicleDialog" header="Nuevo vehículo" modal class="w-full md:w-1/3">
+    <form class="grid gap-2"
+          @submit.prevent="createVehicle">
+      <p class="text-lg text-gray-400 font-medium">Todos los campos son obligatorios</p>
+      <div class="grid w-full">
+        <label class="text-sm">Marca</label>
+        <Dropdown v-model="brand" :options="vehicle_brands" optionLabel="name" placeholder="Selecciona marca" class="w-full" />
+      </div>
+      <div class="grid w-full">
+        <label class="text-sm">Nombre modelo</label>
+        <InputText v-model="model" @input="resetErrorMessage" />
+      </div>
+      <div class="grid w-full">
+        <label class="text-sm">Fecha de fabricación</label>
+        <InputText v-model="manufactureYear" @input="resetErrorMessage" />
+      </div>
+      <div class="grid w-full">
+        <label class="text-sm">URL de foto</label>
+        <InputText v-model="photoUrl" @input="resetErrorMessage" />
+      </div>
+      <p class="text-red-700" v-if="errorMessage">
+        {{ errorMessage }}
+      </p>
+      <Button type="submit" label="Publicar" class="w-full"  />
+    </form>
+  </Dialog>
+
+
+  <Dialog v-model:visible="newCarPostDialog" header="Nuevo post" modal class="w-full md:w-1/3">
     <form class="grid gap-2"
           @submit.prevent="createVehiclePost">
       <p class="text-lg text-gray-400 font-medium">Todos los campos son obligatorios</p>
@@ -125,16 +191,16 @@ export default {
       </div>
       <div class="flex gap-2">
         <div class="grid w-full">
-          <label for="selectedCurrency" class="text-sm">Moneda</label>
+          <label class="text-sm">Moneda</label>
           <Dropdown v-model="currency" :options="currencies" optionLabel="symbol" placeholder="Selecciona moneda" class="w-full" />
         </div>
         <div class="grid w-full">
-          <label for="selectedPrice" class="text-sm">Precio</label>
-          <InputText id="selectedPrice" v-model="price" aria-describedby="username-help" @input="resetErrorMessage" />
+          <label class="text-sm">Precio</label>
+          <InputText v-model="price" aria-describedby="username-help" @input="resetErrorMessage" />
         </div>
       </div>
       <div class="grid w-full">
-        <label for="description" class="text-sm">Descripción</label>
+        <label class="text-sm">Descripción</label>
         <Textarea v-model="description" rows="5" cols="30" @input="resetErrorMessage" />
       </div>
       <p class="text-red-700" v-if="errorMessage">
